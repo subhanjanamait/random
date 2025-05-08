dbca37e960d51c13064e62f1ec882d1243aca8dc
subhanjana123 <100150783+subhanjanamait@users.noreply.github.com>
2025-05-08 14:55:30 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { AddressComponent } from './address/address.component';
+
+@NgModule({
+  declarations: [AppComponent, AddressComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
